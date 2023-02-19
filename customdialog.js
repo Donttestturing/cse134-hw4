ec311d1ee027a4f7f381830ec6119f8f77037c8d
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2023-02-18 20:53:58 -0800

    add some style

--- customdialog.js
+++ customdialog.js
@@ -4,6 +4,9 @@ let dialogs = document.getElementsByTagName('dialog');
 let results = document.getElementsByTagName('result');
 
 buttons[0].addEventListener('click', () =>{
+    let background = document.getElementsByTagName('html');
+    background[0].style = ' background-color:rgb(70, 224, 224);';
+
     results[0].innerHTML = "";
     results[0].style = "";
     dialogs[0].innerHTML = "Alert Pressed! <br> <button id='alertOkButton'>Ok</button> ";
@@ -13,10 +16,15 @@ buttons[0].addEventListener('click', () =>{
     innerOkButton.addEventListener('click', () =>{
         dialogs[0].open = false;
         dialogs[0].innerHTML = "";
+        background[0].style = 'background-color:rgb(200, 224, 224);';
     });
 
 });
 buttons[1].addEventListener('click', () =>{
+    let background = document.getElementsByTagName('html');
+    background[0].style = ' background-color:rgb(110, 224, 224);';
+
+
     results[0].innerHTML = "";
     results[0].style = "";
     dialogs[0].innerHTML = "Do you confirm this? <br> <button id='confirmOkButton'>Ok</button> <button id='confirmCancelButton'>Cancel</button> ";
@@ -26,15 +34,17 @@ buttons[1].addEventListener('click', () =>{
         innerOkButton.addEventListener('click', () =>{
         dialogs[0].open = false;
         dialogs[0].innerHTML = "";
-        results[0].style = 'border: solid 1px black; font-weight:500; background-color:pink';
+        results[0].style = 'border: solid 1px black; font-weight:500; background-color:pink;';
         results[0].innerHTML = confirmResultTemplateStr('true');
+        background[0].style = 'background-color:rgb(200, 224, 224);';
     });
     let innerCancelButton = document.getElementById('confirmCancelButton');
         innerCancelButton.addEventListener('click', () =>{
         dialogs[0].open = false;
         dialogs[0].innerHTML = "";
-        results[0].style = 'border: solid 1px black; font-weight:500; background-color:pink';
+        results[0].style = 'border: solid 1px black; font-weight:500; background-color:pink;';
         results[0].innerHTML = confirmResultTemplateStr('false');
+        background[0].style = 'background-color:rgb(200, 224, 224);';
     });
 
 });
@@ -45,6 +55,9 @@ function promptResultTemplateStr(result){
     return `Prompt result: ${result}`
 }
 buttons[2].addEventListener('click', () =>{
+    let background = document.getElementsByTagName('html');
+    background[0].style = ' background-color:rgb(170, 224, 224);';
+
     results[0].innerHTML = "";
     results[0].style = "";
     dialogs[0].innerHTML = "What is your name? <br> <textarea id='promptTextArea'> </textarea> <br> <button id='promptOkButton'>Ok</button> <button id='promptCancelButton'>Cancel</button> ";
@@ -60,7 +73,7 @@ buttons[2].addEventListener('click', () =>{
         dialogs[0].innerHTML = "";
         results[0].style = 'border: solid 1px black; font-weight:500; background-color:pink';
         results[0].innerHTML = promptResultTemplateStr(txtFromUser);
-
+        background[0].style = 'background-color:rgb(200, 224, 224);';
     });
     let innerCancelButton = document.getElementById('promptCancelButton');
     innerCancelButton.addEventListener('click', () =>{
@@ -68,6 +81,7 @@ buttons[2].addEventListener('click', () =>{
         dialogs[0].innerHTML = "";
         results[0].style = 'border: solid 1px black; font-weight:500; background-color:pink';
         results[0].innerHTML = promptResultTemplateStr('No name given, User hit cancel');
+        background[0].style = 'background-color:rgb(200, 224, 224);';
     });
 
 });
