f2267432f142834f869173a3df1b0ccae2f02e16
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2023-02-20 16:51:38 -0800

    add CRD

---
+++ blog.js
@@ -0,0 +1,154 @@
+    function init() {
+
+        let postTitles = JSON.parse(localStorage.getItem('Post Titles'));
+        let postDate = JSON.parse(localStorage.getItem('Post Dates'));
+        let postSummary = JSON.parse(localStorage.getItem('Post Summaries'));
+
+        if(postTitles === null || postDate === null  || postSummary === null || postSummary.length===0 || postDate.length===0 || postTitles.length===0){
+            localStorage.setItem('Post Titles', JSON.stringify(['First Post', 'Second Post', 'Trials of the Mind']));
+            localStorage.setItem('Post Dates', JSON.stringify(['2023-02-20', '2023-02-21', '2023-02-22']));
+            localStorage.setItem('Post Summaries', JSON.stringify(['I made my first blog post today. It went ok.', 'My second blog post went a lot better than my first. Perhaps Practice makes perfect', 'I tried to set up a CRUD application today, I am feeling like... Well you know what...']));
+
+            postTitles = JSON.parse(localStorage.getItem('Post Titles'));
+            postDate = JSON.parse(localStorage.getItem('Post Dates'));
+            postSummary = JSON.parse(localStorage.getItem('Post Summaries'));
+
+        } 
+
+        loadFromLocalStorage();
+    
+    }
+    init();
+
+    function loadFromLocalStorage(){
+
+        let postTitles = JSON.parse(localStorage.getItem('Post Titles'));
+        let postDate = JSON.parse(localStorage.getItem('Post Dates'));
+        let postSummary = JSON.parse(localStorage.getItem('Post Summaries'));
+
+        let blogPosts = document.getElementsByTagName('output');
+
+        for (let index = 0; index < postTitles.length; index++) {
+            blogPosts[0].innerHTML += '<li>' + postTitles[index] +  ': ' + postDate[index] + ' — ' + postSummary[index]  + "<button class='editButton'> Edit </button> " + "<button class='deleteButton'> Delete </button>"  +  ' </li>';
+
+        }
+        
+
+    }  
+
+    let blogPosts = document.getElementsByTagName('output');
+    let deleteButtons = blogPosts[0].getElementsByClassName('deleteButton')
+
+    for (let index = 0; index < deleteButtons.length; index++) {
+
+        deleteButtons[index].addEventListener('click', () =>{
+            let postTitles = JSON.parse(localStorage.getItem('Post Titles'));
+            let postDates = JSON.parse(localStorage.getItem('Post Dates'));
+            let postSummaries = JSON.parse(localStorage.getItem('Post Summaries'));
+
+            postTitles.splice(index, 1);
+            postDates.splice(index, 1);
+            postSummaries.splice(index, 1);
+
+            localStorage.setItem('Post Titles', JSON.stringify(postTitles));
+            localStorage.setItem('Post Dates', JSON.stringify(postDates));
+            localStorage.setItem('Post Summaries', JSON.stringify(postSummaries));
+
+            blogPosts[0].innerHTML = '';
+            loadFromLocalStorage();
+            window.location.reload();
+
+        });
+        
+    } 
+
+    let editButtons = blogPosts[0].getElementsByClassName('editButton')
+
+    for (let index = 0; index < editButtons.length; index++) {
+
+        editButtons[index].addEventListener('click', () => {
+            console.log('ya');
+
+            let postTitles = JSON.parse(localStorage.getItem('Post Titles'));
+            let postDates = JSON.parse(localStorage.getItem('Post Dates'));
+            let postSummaries = JSON.parse(localStorage.getItem('Post Summaries'));
+
+            postTitles.splice(index, 1);
+            postDates.splice(index, 1);
+            postSummaries.splice(index, 1);
+
+            localStorage.setItem('Post Titles', JSON.stringify(postTitles));
+            localStorage.setItem('Post Dates', JSON.stringify(postDates));
+            localStorage.setItem('Post Summaries', JSON.stringify(postSummaries));
+
+            blogPosts[0].innerHTML = '';
+            loadFromLocalStorage();
+            window.location.reload();
+
+        });
+        
+    } 
+
+
+
+  let addButtons = document.getElementsByClassName('addButton');
+
+  addButtons[0].addEventListener('click', () => {
+    let userDialogField = document.getElementById('userDialogField');
+    userDialogField.open = true;
+    userDialogField.innerHTML = ` <label for="postTitle">Post Title</label>
+                <input type="text" id="postTitle" name="postTitle" required>
+
+                <label for="Date">Date</label>
+                <input type="date" id="Date" name="Date" required>
+
+                <label for="summary">Summary</label> <br>
+                <textarea id="summary" name="summary" required> </textarea>
+                <br>
+                <button id="saveButton">
+                    Save
+                </button>
+                <button id="cancelButton">
+                    Cancel
+                </button>`;
+
+    let saveButton = document.getElementById('saveButton');
+    let cancelButton = document.getElementById('cancelButton');
+
+    cancelButton.addEventListener('click', ()=>{
+        userDialogField.open = false;
+        userDialogField.innerHTML = '';
+    });
+    saveButton.addEventListener('click', ()=>{
+        let postTitleEle = document.getElementById('postTitle');
+        let dateEle = document.getElementById('Date');
+        let summaryEle = document.getElementById('summary');
+
+        const postTitles = JSON.parse(localStorage.getItem('Post Titles'));
+        const postDates = JSON.parse(localStorage.getItem('Post Dates'));
+        const postSummaries = JSON.parse(localStorage.getItem('Post Summaries'));
+
+        postTitles.push(postTitleEle.value);
+        postDates.push(dateEle.value);
+        postSummaries.push(summaryEle.value);
+
+        localStorage.setItem('Post Titles', JSON.stringify(postTitles));
+        localStorage.setItem('Post Dates', JSON.stringify(postDates));
+        localStorage.setItem('Post Summaries', JSON.stringify(postSummaries));
+    
+        userDialogField.open = false;
+        userDialogField.innerHTML = '';
+
+        document.getElementsByTagName('output')[0].innerHTML = '';
+        loadFromLocalStorage();
+
+    });
+
+
+
+
+  });
+
+
+
+
